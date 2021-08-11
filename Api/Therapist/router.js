b8899486aceed1697182bf4611089b8ea716fe2e
Liketodev3 <liketodev3@gmail.com>
2021-08-11 16:47:16 +0400

    new register update

--- Api/Therapist/router.js
+++ Api/Therapist/router.js
@@ -14,11 +14,13 @@ const upload = multer({ storage: storage, fileFilter: fileFilter })
         name: 'COSHH_certificate', maxCount: 1,
     }]);
 
+const registerUpload = multer({storage: storage, fileFilter: fileFilter}).single('certificate')
+
 therapist.get('/log/getYourCreditCard', controllers.retrieveCard);
 therapist.get('/log/getYourGiftCards', controllers.getYourGiftCards);
 therapist.post('/registerStepOne', validation.stepOneValidation, controllers.registerStepOne);
 therapist.post('/registerStepTwo', validation.stepTwoValidation, controllers.registerStepTwo);
-therapist.post('/registerFinishStep', validation.finishStepValidation, controllers.registerFinishStep);
+therapist.post('/registerFinishStep', registerUpload, validation.finishStepValidation, controllers.registerFinishStep);
 therapist.post('/log/addProduct', upload, controllers.addProduct);
 therapist.post('/log/addGiftCard', validation.giftValidation, controllers.addGiftCards);
 therapist.post('/log/addNewCard', controllers.addNewCard);
