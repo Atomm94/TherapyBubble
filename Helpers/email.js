d8f6c66a2c96c4179b3fd1e03e1d65ac8081a38e
Liketodev3 <liketodev3@gmail.com>
2021-08-13 13:21:32 +0400

    new update

--- Helpers/email.js
+++ Helpers/email.js
@@ -4,28 +4,31 @@ const OAuth2 = google.auth.OAuth2;
 import smsCode from 'generate-sms-verification-code';
 import * as config from '../config';
 
-const oauth2Client = new OAuth2(process.env.CLIENT_ID, process.env.CLIENT_ID, "https://developers.google.com/oauthplayground");
+const oauth2Client = new OAuth2("993202546190-v8qjss1vn59e52s0o9dmr3kqph1rbnmb.apps.googleusercontent.com", "993202546190-v8qjss1vn59e52s0o9dmr3kqph1rbnmb.apps.googleusercontent.com", "https://developers.google.com/oauthplayground");
 
-oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+oauth2Client.setCredentials({ refresh_token: "1//04Ii4ijHLJA9uCgYIARAAGAQSNwF-L9Ir20xpjbLxi4iiDXe80-lFnVOrtq2l4WhBRGKJas1H5Lyt7dUlhjIuOvcyUKj_Z1xoXAQ" });
 
 
 const send_mail = mailer.createTransport({
     service: 'gmail',
     auth: {
         type: "OAuth2",
-        user: process.env.EMAIL,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
+        user: "ilovecoding777@gmail.com",
+        clientId: "993202546190-v8qjss1vn59e52s0o9dmr3kqph1rbnmb.apps.googleusercontent.com",
+        clientSecret: "xaGswMU2EJFOy7h1VByOubUE",
+        refreshToken: "1//04Ii4ijHLJA9uCgYIARAAGAQSNwF-L9Ir20xpjbLxi4iiDXe80-lFnVOrtq2l4WhBRGKJas1H5Lyt7dUlhjIuOvcyUKj_Z1xoXAQ",
+        access_type:"offline",
+        grantType:"authorization_code",
         accessToken: oauth2Client.getAccessTokenAsync
     }
 });
+
 
 const send = async (to, type) => {
     const generatedCode = smsCode(6, {type: 'string'})
     new Promise((res, rej) => {
         send_mail.sendMail({
-            from: process.env.EMAIL,
+            from: "ilovecoding777@gmail.com",
             to: to,
             html:
                 '<div>' +
